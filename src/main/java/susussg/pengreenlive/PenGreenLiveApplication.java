c63f6557c882d654bae7a66d4d4ef4d702c8565e
hinata7010 <1234minseok@naver.com>
2024-04-22 10:34:02 +0900

    first commit

---
+++ src/main/java/susussg/pengreenlive/PenGreenLiveApplication.java
@@ -0,0 +1,13 @@
+package susussg.pengreenlive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PenGreenLiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PenGreenLiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/susussg/pengreenlive/PengreenliveApplicationTests.java
@@ -0,0 +1,13 @@
+package susussg.pengreenlive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PengreenliveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
