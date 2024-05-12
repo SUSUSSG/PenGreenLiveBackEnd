2374d9ad80c751d5cc015e1db40c55f02dc1a453
zzzjinwook <zzzjinwook@naver.com>
2024-05-12 22:53:12 +0900

    feat : UUID로 유저 이름 조회

---
+++ src/main/java/susussg/pengreenlive/user/Service/UserService.java
@@ -0,0 +1,6 @@
+package susussg.pengreenlive.user.Service;
+
+public interface UserService {
+    String getUserNmByUUID(String uuid);
+
+}
---
+++ src/main/java/susussg/pengreenlive/user/Service/UserServiceImpl.java
@@ -0,0 +1,17 @@
+package susussg.pengreenlive.user.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import susussg.pengreenlive.user.Mapper.UserMapper;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+    @Autowired
+    UserMapper userMapper;
+
+    @Override
+    public String getUserNmByUUID(String uuid) {
+        return userMapper.selectUserNmByUUID(uuid); //세션의 유저 아이디값을 인자로 받음
+    }
+}
