2806747b864da418986740250e36fd58c14229fe
zzzjinwook <zzzjinwook@naver.com>
2024-05-12 16:37:28 +0900

    feat : 스프링 스케줄러 클래스 생성

---
+++ src/main/java/susussg/pengreenlive/config/SchedulerConfig.java
@@ -0,0 +1,9 @@
+package susussg.pengreenlive.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulerConfig {
+}
