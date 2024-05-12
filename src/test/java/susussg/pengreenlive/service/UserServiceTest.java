cc5cee2bcfaf3b052c9343f8c6f1c655574c808f
zzzjinwook <zzzjinwook@naver.com>
2024-05-12 22:53:24 +0900

    test : 유저 이름 조회 테스트

---
+++ src/test/java/susussg/pengreenlive/service/UserServiceTest.java
@@ -0,0 +1,26 @@
+package susussg.pengreenlive.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import susussg.pengreenlive.user.Service.UserService;
+import susussg.pengreenlive.util.DTO.BanwordValidationResultDTO;
+
+@Log4j2
+@SpringBootTest
+public class UserServiceTest {
+
+    @Autowired
+    UserService userService;
+
+    @Test
+    public void testGetUserNmByUUID() {
+        String testUUID = "a1b2c3d4-e5f6-g7h8-i9j0-k1l2m3n4o5p6";
+
+        String userNm = userService.getUserNmByUUID(testUUID);
+        log.info(userNm);
+    }
+}
