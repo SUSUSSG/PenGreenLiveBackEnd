3e8359aca06d1a6fe52845ee1b0a286ee2017482
장서윤 <seoy316@naver.com>
2024-05-13 15:50:26 +0900

    feat: Order 엔티티 생성

---
+++ src/main/java/susussg/pengreenlive/order/domain/Order.java
@@ -0,0 +1,33 @@
+package susussg.pengreenlive.order.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@ToString
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "TB_ORDER")
+public class Order {
+
+    @Id @GeneratedValue
+    @Column(name="order_seq")
+    long id;
+
+    String userUUID;
+    long productSeq;
+    int orderQty;
+    String orderPayment;
+    LocalDateTime orderDate;
+    int orderProductPrice;
+    int orderPayedPrice;
+    long broadcastSeq;
+    String deliveryStatus;
+    boolean reivewYn;
+    long vendorSeq;
+    long channelSeq;
+
+}
