898402620e25382296f500a3f8b2410dad069cc6
hinata7010 <1234minseok@naver.com>
2024-05-14 12:38:23 +0900

    feat : 방송 통계 생성, 방송 통계 조회, 좋아요 증감, 평균 시청자수 최대 시청자수 업데이트 기능 추가

---
+++ src/main/java/susussg/pengreenlive/broadcast/service/BroadcastStatisticsServiceImpl.java
@@ -0,0 +1,51 @@
+package susussg.pengreenlive.broadcast.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import susussg.pengreenlive.broadcast.dto.BroadcastStatistics;
+import susussg.pengreenlive.broadcast.mapper.BroadcastStatisticsMapper;
+import susussg.pengreenlive.broadcast.service.BroadcastStatisticsService;
+
+@Service
+public class BroadcastStatisticsServiceImpl implements BroadcastStatisticsService {
+
+    @Autowired
+    private BroadcastStatisticsMapper broadcastStatisticsMapper;
+
+    @Override
+    @Transactional
+    public void insertBroadcastStatistics(BroadcastStatistics broadcastStatistics) {
+        broadcastStatisticsMapper.insertBroadcastStatistics(broadcastStatistics);
+    }
+
+    @Override
+    @Transactional
+    public void updateLikesCount(long broadcastSeq) {
+        broadcastStatisticsMapper.updateLikesCount(broadcastSeq);
+    }
+
+    @Override
+    @Transactional
+    public void decrementLikesCount(long broadcastSeq) {
+        broadcastStatisticsMapper.decrementLikesCount(broadcastSeq);
+    }
+
+    @Override
+    @Transactional
+    public void updateAverageViewerCount(long broadcastSeq, int averageViewerCount) {
+        broadcastStatisticsMapper.updateAverageViewerCount(broadcastSeq, averageViewerCount);
+    }
+
+    @Override
+    @Transactional
+    public void updateMaxViewerCount(long broadcastSeq, int maxViewerCount) {
+        broadcastStatisticsMapper.updateMaxViewerCount(broadcastSeq, maxViewerCount);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public BroadcastStatistics findById(long broadcastSeq) {
+        return broadcastStatisticsMapper.findById(broadcastSeq);
+    }
+}
