0c45db03ce2e0a30775a1ea1f322734d85520dda
JiniiW <ksjine2@gmail.com>
2024-05-18 12:46:45 +0900

    feat : 라이브 방송 정보 DTO 구현

---
+++ src/main/java/susussg/pengreenlive/broadcast/dto/LiveBroadcastInfoDTO.java
@@ -0,0 +1,28 @@
+package susussg.pengreenlive.broadcast.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class LiveBroadcastInfoDTO {
+    //방송
+    private BroadcastDTO broadcast;
+
+    //공지
+    private List<NoticeDTO> notices;
+
+    //질문과 답변
+    private List<FaqDTO> faqs;
+
+    //라이브 혜택
+    private List<BenefitDTO> benefits;
+
+    //판매 중인 상품
+}
