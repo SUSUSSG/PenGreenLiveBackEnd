9ceb12638acd32e784380ee1190310265c16a120
JiniiW <ksjine2@gmail.com>
2024-05-18 12:46:57 +0900

    feat : 라이브 방송 정보 매퍼 구현

---
+++ src/main/java/susussg/pengreenlive/broadcast/mapper/LiveBroadcastInfoMapper.java
@@ -0,0 +1,21 @@
+package susussg.pengreenlive.broadcast.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import susussg.pengreenlive.broadcast.dto.BenefitDTO;
+import susussg.pengreenlive.broadcast.dto.BroadcastDTO;
+import susussg.pengreenlive.broadcast.dto.FaqDTO;
+import susussg.pengreenlive.broadcast.dto.NoticeDTO;
+
+import java.util.List;
+
+@Mapper
+public interface LiveBroadcastInfoMapper {
+    BroadcastDTO selectBroadcast(long broadcastId);
+
+    List<NoticeDTO> selectNotice(long broadcastId);
+
+    List<BenefitDTO> selectBenefit(long broadcastId);
+
+    List<FaqDTO> selectFaq(long broadcastId);
+
+}
