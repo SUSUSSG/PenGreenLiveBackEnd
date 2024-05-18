1ae2db55c8552c5bdfd3b06e8d20069ea46c8897
zzzjinwook <zzzjinwook@naver.com>
2024-05-18 16:32:48 +0900

    Revert "feat : csrf 토큰 문제 gpt 경로 예외처리"
    This reverts commit 25a448597f35708c2d0e1bc9b4e802fed7fd1efe.

--- src/main/java/susussg/pengreenlive/PenGreenLiveApplication.java
+++ src/main/java/susussg/pengreenlive/PenGreenLiveApplication.java
@@ -2,11 +2,8 @@ package susussg.pengreenlive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
-import susussg.pengreenlive.config.CustomObjectMapperConfig;
 
 @SpringBootApplication
-@Import(CustomObjectMapperConfig.class)
 public class PenGreenLiveApplication {
 
 	public static void main(String[] args) {
--- src/main/java/susussg/pengreenlive/config/RedisConfig.java
+++ src/main/java/susussg/pengreenlive/config/RedisConfig.java
@@ -1,18 +1,16 @@
 package susussg.pengreenlive.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import org.springframework.context.annotation.Primary;
 
 @Configuration
 @EnableRedisRepositories
@@ -38,21 +36,19 @@ public class RedisConfig {
     }
 
     @Bean
-    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory, ObjectMapper customObjectMapper) {
+    public StringRedisTemplate stringRedisTemplate() {
         StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
-        stringRedisTemplate.setConnectionFactory(redisConnectionFactory);
 
-        GenericJackson2JsonRedisSerializer customSerializer = new GenericJackson2JsonRedisSerializer(customObjectMapper);
+        stringRedisTemplate.setConnectionFactory(redisConnectionFactory());
 
         stringRedisTemplate.setKeySerializer(new StringRedisSerializer());
-        stringRedisTemplate.setValueSerializer(customSerializer);
-        stringRedisTemplate.setDefaultSerializer(customSerializer);
+        stringRedisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        stringRedisTemplate.setDefaultSerializer(new StringRedisSerializer());
         stringRedisTemplate.afterPropertiesSet();
         return stringRedisTemplate;
     }
-
     @Bean
-    public RedisSerializer<Object> springSessionDefaultRedisSerializer(ObjectMapper customObjectMapper) {
-        return new GenericJackson2JsonRedisSerializer(customObjectMapper);
+    public RedisSerializer<Object> springSessionDefaultRedisSerializer() {
+        return new GenericJackson2JsonRedisSerializer();
     }
 }
