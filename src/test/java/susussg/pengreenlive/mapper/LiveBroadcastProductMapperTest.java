873fa5e9a6cc5775baf67e07d26fcf2c3717c02f
JiniiW <ksjine2@gmail.com>
2024-05-19 22:11:17 +0900

    test : 테스트 코드 수정

--- src/test/java/susussg/pengreenlive/mapper/LiveBroadcastProductMapperTest.java
+++ src/test/java/susussg/pengreenlive/mapper/LiveBroadcastProductMapperTest.java
@@ -2,14 +2,12 @@ package susussg.pengreenlive.mapper;
 
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import susussg.pengreenlive.broadcast.dto.LiveBroadcastProductDTO;
 import susussg.pengreenlive.broadcast.mapper.LiveBroadcastMapper;
 
 import java.util.List;
-import java.util.Map;
 
 @SpringBootTest
 @Log4j2
@@ -24,9 +22,5 @@ public class LiveBroadcastProductMapperTest {
         log.info(productList.toString());
 
     }
-    @Test
-    public void selectLabelImages() {
-        List<Map<Long, String>> labelImages = liveBroadcastMapper.selectLabelImages(11);
-        log.info(labelImages);
-    }
+
 }
