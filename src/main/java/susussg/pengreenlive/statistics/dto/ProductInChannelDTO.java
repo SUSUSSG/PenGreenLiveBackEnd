1d62d52259de01aaa805711da6a6a0d8a7a9d6b4
zzzjinwook <zzzjinwook@naver.com>
2024-05-21 21:45:17 +0900

    feat : 분석 시 데이터 부족 예외 처리

--- src/main/java/susussg/pengreenlive/statistics/dto/ProductInChannelDTO.java
+++ src/main/java/susussg/pengreenlive/statistics/dto/ProductInChannelDTO.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 @Data
 public class ProductInChannelDTO {
+    private Long productSeq;
     private String productNm;
     private String productCd;
     private int totalSales;
