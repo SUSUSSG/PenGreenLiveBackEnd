1ad5278e423f8cbb7b41490ec8c24985329a8ce9
zzzjinwook <zzzjinwook@naver.com>
2024-05-21 22:14:05 +0900

    refactor : 파이썬 경로 프로퍼티화

--- src/main/java/susussg/pengreenlive/statistics/service/PythonServiceImpl.java
+++ src/main/java/susussg/pengreenlive/statistics/service/PythonServiceImpl.java
@@ -2,6 +2,7 @@ package susussg.pengreenlive.statistics.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import susussg.pengreenlive.statistics.service.PythonService;
 
@@ -12,21 +13,27 @@ import java.util.Map;
 @Log4j2
 public class PythonServiceImpl implements PythonService {
 
+    @Value("${python.env.url}")
+    private String PATH;
+
     @Override
-    public Map<String, String> generateImage(String review) throws IOException, InterruptedException {
+    public Map<String, String> generateImage(String review)
+        throws IOException, InterruptedException {
         String pythonScriptPath = "src/main/java/susussg/pengreenlive/statistics/py/word_network.py";
 
         ObjectMapper objectMapper = new ObjectMapper();
         String reviewJson = objectMapper.writeValueAsString(review);
 
-        String pythonPath = "/usr/local/bin/python3"; // TODO : 배포 시 서버 내 주소로 변경
+        String pythonPath = PATH; // TODO : 배포 시 서버 내 주소로 변경
 
-        ProcessBuilder processBuilder = new ProcessBuilder(pythonPath, pythonScriptPath, reviewJson);
+        ProcessBuilder processBuilder = new ProcessBuilder(pythonPath, pythonScriptPath,
+            reviewJson);
         processBuilder.redirectErrorStream(true);
         Process process = processBuilder.start();
 
         StringBuilder output = new StringBuilder();
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+        try (BufferedReader reader = new BufferedReader(
+            new InputStreamReader(process.getInputStream()))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 output.append(line);
@@ -35,7 +42,8 @@ public class PythonServiceImpl implements PythonService {
 
         int exitCode = process.waitFor();
         if (exitCode != 0) {
-            throw new RuntimeException("Python script execution failed with exit code: " + exitCode);
+            throw new RuntimeException(
+                "Python script execution failed with exit code: " + exitCode);
         }
 
         return objectMapper.readValue(output.toString(), Map.class);
