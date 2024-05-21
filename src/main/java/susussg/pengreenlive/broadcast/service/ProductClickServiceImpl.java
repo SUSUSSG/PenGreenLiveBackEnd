adb139d9550f896a3b0395f3e7f13cdbeac6dc98
hinata7010 <1234minseok@naver.com>
2024-05-21 19:53:55 +0900

    refactor : 테이블 통합에 필요없는 메소드 주석 처리

--- src/main/java/susussg/pengreenlive/broadcast/service/ProductClickServiceImpl.java
+++ src/main/java/susussg/pengreenlive/broadcast/service/ProductClickServiceImpl.java
@@ -3,11 +3,7 @@ package susussg.pengreenlive.broadcast.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import susussg.pengreenlive.broadcast.dto.BroadcastProductDTO;
-import susussg.pengreenlive.broadcast.dto.ProductClick;
 import susussg.pengreenlive.broadcast.mapper.ProductClicksMapper;
-
-import java.util.List;
 
 @Service
 public class ProductClickServiceImpl implements ProductClickService{
@@ -15,29 +11,29 @@ public class ProductClickServiceImpl implements ProductClickService{
     @Autowired
     private ProductClicksMapper productClickMapper;
 
-    @Override
-    public void addProductClick(Long broadcastSeq, Long productSeq) {
-        ProductClick productClick = ProductClick.builder()
-                .broadcastSeq(broadcastSeq)
-                .productSeq(productSeq)
-                .build();
+//    @Override
+//    public void addProductClick(Long broadcastSeq, Long productSeq) {
+//        ProductClick productClick = ProductClick.builder()
+//                .broadcastSeq(broadcastSeq)
+//                .productSeq(productSeq)
+//                .build();
+//
+//        productClickMapper.insertProductClick(productClick);
+//    }
 
-        productClickMapper.insertProductClick(productClick);
-    }
-
-    @Override
-    @Transactional
-    public void addProductClicksForBroadcast(Long broadcastSeq) {
-        List<BroadcastProductDTO> broadcastProducts = productClickMapper.getProductsByBroadcastSeq(broadcastSeq);
-
-        for (BroadcastProductDTO broadcastProduct : broadcastProducts) {
-            ProductClick productClick = new ProductClick();
-            productClick.setBroadcastSeq(broadcastProduct.getBroadcastSeq());
-            productClick.setProductSeq(broadcastProduct.getProductSeq());
-
-            productClickMapper.insertProductClick(productClick);
-        }
-    }
+//    @Override
+//    @Transactional
+//    public void addProductClicksForBroadcast(Long broadcastSeq) {
+//        List<BroadcastProductDTO> broadcastProducts = productClickMapper.getProductsByBroadcastSeq(broadcastSeq);
+//
+//        for (BroadcastProductDTO broadcastProduct : broadcastProducts) {
+//            ProductClick productClick = new ProductClick();
+//            productClick.setBroadcastSeq(broadcastProduct.getBroadcastSeq());
+//            productClick.setProductSeq(broadcastProduct.getProductSeq());
+//
+//            productClickMapper.insertProductClick(productClick);
+//        }
+//    }
 
     @Override
     @Transactional
