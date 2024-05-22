561e70a9574b4295d270b08543e6f64e8be066c4
zzzjinwook <zzzjinwook@naver.com>
2024-05-22 20:14:41 +0900

    feat : 일자별 감정 분석 mapper 작성

--- src/main/java/susussg/pengreenlive/naver/mapper/ReviewSummaryMapper.java
+++ src/main/java/susussg/pengreenlive/naver/mapper/ReviewSummaryMapper.java
@@ -1,11 +1,13 @@
 package susussg.pengreenlive.naver.mapper;
 
 import java.util.List;
+import java.util.Map;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface ReviewSummaryMapper {
 
   List<String> getReviewContentsByProductSeq(Long productSeq);
+  List<Map<String, Object>> getReviewContentsByProductSeqAndDate(Long productSeq);
 
 }
