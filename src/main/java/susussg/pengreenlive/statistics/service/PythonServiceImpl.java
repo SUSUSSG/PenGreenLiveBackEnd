4b64b3aa2cf3021aa5e7496d0df7dce0c943be16
zzzjinwook <zzzjinwook@naver.com>
2024-05-22 12:24:11 +0900

    fix : 경로 수정

--- src/main/java/susussg/pengreenlive/statistics/service/PythonServiceImpl.java
+++ src/main/java/susussg/pengreenlive/statistics/service/PythonServiceImpl.java
@@ -18,7 +18,7 @@ public class PythonServiceImpl implements PythonService {
 
     @Override
     public Map<String, String> generateImage(String review) throws IOException, InterruptedException {
-        String pythonScriptPath = "opt/susussg-backend/word_network.py";
+        String pythonScriptPath = "/opt/susussg-backend/word_network.py";
 
         ObjectMapper objectMapper = new ObjectMapper();
         String reviewJson = objectMapper.writeValueAsString(review);
