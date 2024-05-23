d670f47582771e080cea6bee4ef27ac1c8dd81f8
JiniiW <ksjine2@gmail.com>
2024-05-23 11:05:36 +0900

    style : 주석 제거

--- src/main/java/susussg/pengreenlive/broadcast/dto/BroadcastTimeDTO.java
+++ src/main/java/susussg/pengreenlive/broadcast/dto/BroadcastTimeDTO.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.cglib.core.Local;
 
 import java.time.LocalDateTime;
 
