866808e54f6ffd61a265393c14e190a1cae34d57
장서윤 <seoy316@naver.com>
2024-05-26 22:00:45 +0900

    fix: 쿠키 허용 및 허용할 오리진 지정

--- src/main/java/susussg/pengreenlive/config/CorsConfig.java
+++ src/main/java/susussg/pengreenlive/config/CorsConfig.java
@@ -14,10 +14,12 @@ public class CorsConfig {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // 모든 URL 패턴
-            .allowedOriginPatterns("*")  // Vue.js 서버의 URL과 포트
+//            .allowedOriginPatterns("*")  // Vue.js 서버의 URL과 포트
+            .allowedOrigins("http://localhost:5173")  // 허용할 오리진
             .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")  // 모든 HTTP 메소드 허용
             .allowedHeaders("*")  // 모든 헤더 허용
-            .allowCredentials(false)  // 쿠키 및 인증 정보 포함 허용
+
+            .allowCredentials(true)  // 쿠키 및 인증 정보 포함 허용
             .maxAge(3600);  // 3600초 동안 pre-flight(사전 전송 검사) 결과 캐시
       }
     };
