73a9a1de20d3527023433dcfe32a3c436db31f7e
장서윤 <seoy316@naver.com>
2024-05-29 09:23:42 +0900

    file: 삭제

--- src/main/java/susussg/pengreenlive/chat/config/WebSocketConfiguration.java
+++ src/main/java/susussg/pengreenlive/chat/config/WebSocketConfiguration.java
@@ -1,16 +1,12 @@
 package susussg.pengreenlive.chat.config;
 
-import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import susussg.pengreenlive.chat.handler.MyHandler;
-import susussg.pengreenlive.chat.interceptor.CustomHandshakeInterceptor;
 
 @Configuration
 @EnableWebSocket
