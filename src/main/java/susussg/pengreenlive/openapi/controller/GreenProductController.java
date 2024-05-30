02490d9a2509d613e993ac82192c00894fc28b4c
zzzjinwook <zzzjinwook@naver.com>
2024-05-30 10:58:41 +0900

    style : swagger 추가 작성

--- src/main/java/susussg/pengreenlive/openapi/controller/GreenProductController.java
+++ src/main/java/susussg/pengreenlive/openapi/controller/GreenProductController.java
@@ -1,5 +1,6 @@
 package susussg.pengreenlive.openapi.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.extern.log4j.Log4j2;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,7 @@ public class GreenProductController {
     @Autowired
     private GreenProductService greenProductService;
 
+    @Operation(summary = "녹색인증 정보를 조회합니다.", description = "녹색인증코드를 받아 정보와 유효성을 확인합니다.")
     @GetMapping("/green-product")
     public String getGreenProductInfo(@RequestParam String prodIxid) {
         String productInfo = greenProductService.getGreenProductInfo(prodIxid);
